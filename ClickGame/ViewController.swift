66b6c30e51548d5269c28fd6405ec734c5165e1d
Will Xia <gicalc32@gmail.com>
2018-09-01 15:31:00 -0400

    Import Files to Github

---
+++ ClickGame/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by William Xia on 8/28/18.
+//  Copyright © 2018 William Xia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var player1ScoreLabel: UILabel!
+    @IBOutlet weak var player2ScoreLabel: UILabel!
+    
+    var player1Score = 0
+    var player2Score = 0
+    var isGameStarted = false
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+        isGameStarted = true
+        
+        player1Score = 0
+        player2Score = 0
+        updateUI()
+        
+        Timer.scheduledTimer(withTimeInterval: 10, repeats: false) { (_) in
+            sender.isEnabled = true
+            self.isGameStarted = false
+        }
+    }
+    
+    @IBAction func player1ButtonPressed(_ sender: Any) {
+        player1Score += 1
+        updateUI()
+    }
+    
+    @IBAction func player2ButtonPressed(_ sender: Any) {
+        player2Score += 1
+        updateUI()
+    }
+ 
+    func updateUI() {
+        if isGameStarted {
+            player1ScoreLabel.text = "Player 1 Score: \(player1Score)"
+            player2ScoreLabel.text = "Player 2 Score: \(player2Score)"
+        }
+        
+    }
+
+}
+
